be904b8128bda06bf56e67a07b5b61fa7843423a
Tom Johnson <tomajohnson21@gmail.com>
2019-04-23 19:52:39 -0500

    Created function to handle cards being clicked. The function will check if the card has been clicked for and respond accordingly, then shuffle the cards

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
 import Wrapper from './components/Wrapper';
 import Card from './components/Card'
-import Footer from './components/Footer'
+import shuffle from './components/shuffle'
 import cards from './cards.json'
 
 class App extends Component {
@@ -14,7 +14,10 @@ class App extends Component {
     cards
   }
 
+  newGame = () => {
 
+    this.setState({currentScore: 0, cards: shuffle(cards)})
+  }
 
   handleStateChange = event => {
 
@@ -26,13 +29,35 @@ class App extends Component {
   }
 
   handleCardClick = id => {
-  
-    console.log(id)
+    let selectedIndex = this.state.cards.findIndex(card => card.id === id);
+    let selectedCard = this.state.cards[selectedIndex];
+
+
+    if(selectedCard.clicked){
+      alert("You have already clicked this card! Game over!");
+      this.newGame();
+    } else {
+      let score = this.state.currentScore + 1;
+      if(score > this.state.highScore){
+        this.setState({highScore: score})
+      }
+      this.state.cards[selectedIndex].clicked = true
+      this.setState({currentScore: score})
+    }
+
+    if(this.state.currentScore >= 11){
+
+      alert("You've reached the maximum points, you win!");
+      this.newGame();
+    } else {
+
+      this.setState({cards: shuffle(cards)});
+    }
   }
 
   componentDidMount() {
 
-
+    this.newGame();
   }
   
   render() {
